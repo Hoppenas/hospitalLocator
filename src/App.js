fe01beae8a348db3b1b8bf198beb6057cde41c52
Hoppenas <martynas.hoppenas@gmail.com>
2022-10-30 21:52:29 +0200

    ADD: basic search

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { TextField, Grid, IconButton, InputAdornment, Paper, Typography } from "@mui/material";
+import React, { useState } from "react";
 
 function App() {
+  const [city, setCity] = useState('');
+  const [searchLocation, setSearchLocation] = useState('');
+
+  const hospitalList = {
+    ligonines: [
+       {
+          name: "Vilnius university hospital",
+          miestas: "Vilnius",
+          adresas: "Antakalnio g. 57",
+          telefonas: "(8-5) 216 9069"
+       },
+       {
+          name: "Vilnius City Clinical Hospital",
+          miestas: "Vilnius",
+          adresas: "Antakalnio g. 57",
+          telefonas: "(8-5) 234 4519"
+       },
+       {
+          name: "Vilnius City Clinical Hospital",
+          miestas: "Vilnius",
+          adresas: "Antakalnio g. 57",
+          telefonas: "(8-5) 234 4519"
+       },
+       {
+          name: "Vilnius University Hospital Santaros klinikos",
+          miestas: "Vilnius",
+          adresas: "Santariškių g. 2",
+          telefonas: "(8-5) 236 5000"
+       },
+       {
+          name: "Clinical Hospital of Kaunas",
+          miestas: "Kaunas",
+          adresas: "Josvainių g. 2",
+          telefonas: "(8-37) 306000"
+       },
+       {
+          name: "Republican Panevezys Hospital",
+          miestas: "Panevezys",
+          adresas: "Smėlynės g. 25",
+          telefonas:"(8-45) 507184"
+       }
+    ]
+ };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container justifyContent='center'>
+      <Grid item margin='10px'>
+        <TextField id="filled-basic" label="Pld enter your location" variant="filled" onChange={(value) => setCity(value.target.value)} InputProps={{
+    endAdornment: (
+      <InputAdornment position="end">
+        <IconButton edge="end" color="primary" onClick={() => setSearchLocation(city)}>
+          Search
+        </IconButton>
+      </InputAdornment>
+    ),
+  }} />
+      </Grid>
+      <Grid item container xs={12}>
+        {hospitalList.ligonines.map((hospital, id) => 
+        hospital.miestas.toLocaleLowerCase().includes(searchLocation.toLocaleLowerCase()) && 
+        searchLocation !== '' &&
+          <Grid item container xs={3} margin='15px'>
+            <Paper elevation={3} key={id} margin='10px'>
+            <Typography item>
+              {hospital.name}
+            </Typography>
+            <Typography item>
+              {hospital.adresas}
+            </Typography>
+            <Typography item>
+              {hospital.miestas}
+            </Typography>
+            <Typography item>
+              {hospital.telefonas}
+            </Typography>
+          </Paper>
+          </Grid>
+        )}
+      </Grid>
+    </Grid>
   );
 }
 
